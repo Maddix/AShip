2d431ee2da768bcd1430308cff30c64314b29033
Maddix <starcraftd@gmail.com>
2015-08-09 18:32:27 -0400

    Made progress on documentation
    Split everything up into a readable format. Just need to parse it and
    get what I need.

--- easyServer/documentation/doc.py
+++ easyServer/documentation/doc.py
@@ -9,34 +9,35 @@ def loadFile(path, filename, operation="r"):
 	return file
 
 
-def find_in_line(line, text):
+def find(line, text):
 	if line.find(text) != -1:
 		return True
 
-def get_functions(line):
-	funcs = {}
-	if find_in_line(line, "function "):
-		name = line[:line.find("(")].strip().split(" ")[-1]
-		params = get_func_params(line)
-		funcs[name] = params
-	elif find_in_line(line, " function"):
-		split = line.split("=")
-		name = split[0].split(".")[1]
-		params = get_func_params(split[1])
-		funcs[name] = params
+known_functions = {}
 
-		return funcs
-	return False
-
-def get_func_params(line):
-	""" Return found parameters """
-	first_paren = line.find("(")
-	second_paren = line.rfind(")")
-	if first_paren != -1 or second_paren != -1:
-		return [param.strip() for param in line[first_paren + 1 : second_paren].split(",")]
-	else:
-		return False
+complete = []
 
 
 for line in loadFile("", "test_file.txt"):
-	print(line.strip("\n").strip("\t").strip("//"))
+	cleaned = []
+
+	replace_list = ["(", ")", ",", ".", ":", "{", "}", "[", "]", "=", "+", "-", ";", "//", "/*", "*/"]
+
+	clean = line.strip().strip("\n")
+
+	for item in replace_list:
+		clean = clean.replace(item, " " + item + " ")
+
+	clean = clean.split(" ")
+
+	for item in clean:
+		if item is not "":
+			cleaned.append(item)
+
+	if cleaned != []:
+		complete.extend(cleaned)
+
+print(complete)
+
+
+
