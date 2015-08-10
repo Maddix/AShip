79bbfb2984aa3cb2eaa28ff048b3606acf196cdb
Maddix <starcraftd@gmail.com>
2015-08-10 16:48:58 -0400

    Made more progress to the documentation script

--- easyServer/documentation/doc.py
+++ easyServer/documentation/doc.py
@@ -2,42 +2,72 @@
 
 import sys
 
-def loadFile(path, filename, operation="r"):
-	file = []
-	with open(path + filename, operation) as open_file:
-		file = open_file.readlines()
-	return file
-
+PAD_LIST = ["(", ")", ",", ".", ":", "{", "}", "[", "]", "=", "+", "-", ";", "//", "/*", "*/"]
 
 def find(line, text):
 	if line.find(text) != -1:
 		return True
 
-known_functions = {}
+def load_file(path, filename, operation="r"):
+	file = []
+	with open(path + filename, operation) as open_file:
+		file = open_file.readlines()
+	return file
 
-complete = []
+def parse_file(loaded_file, pad_list):
+	parsed = []
 
+	for line in loaded_file:
+		clean = line.strip().strip("\n")
+		cleaned = []
 
-for line in loadFile("", "test_file.txt"):
-	cleaned = []
+		for item in pad_list:
+			clean = clean.replace(item, " " + item + " ") # Should I use str.format here?
 
-	replace_list = ["(", ")", ",", ".", ":", "{", "}", "[", "]", "=", "+", "-", ";", "//", "/*", "*/"]
+		clean = clean.split(" ")
+		# Keep track if we pass the single line comment syntax
+		isComment = False
+		for item in clean:
+			if item is not "":
+				# Special for single line comments
+				if isComment:
+					item = "//" + item
+				if item.strip() == "//":
+					isComment = True
+				cleaned.append(item)
 
-	clean = line.strip().strip("\n")
+		if cleaned != []:
+			parsed.extend(cleaned)
 
-	for item in replace_list:
-		clean = clean.replace(item, " " + item + " ")
+	return parsed
 
-	clean = clean.split(" ")
+# parsed_file should be reversed at least when you call it from outside the function
+# parent_objects should be a dict
+def format_parsed_file(parsed_file):
+	tree = {}
 
-	for item in clean:
-		if item is not "":
-			cleaned.append(item)
+	bracket_pairs = []
+	pair = []
 
-	if cleaned != []:
-		complete.extend(cleaned)
+	for index in range(len(parsed_file)):
+		word = parsed_file[index]
 
-print(complete)
+		if word == "{":
+			pair.append(index)
+		elif word == "}":
+			pair.append(index)
 
+		if len(pair) == 2:
+			bracket_pairs.append(pair)
+			pair = []
 
+	print(bracket_pairs)
 
+	return tree
+
+#print(format_parsed_file(parse_file(load_file("", "test_file.txt"), PAD_LIST).reverse()))
+parsed = parse_file(load_file("", "test_file.txt"), PAD_LIST)
+print(parsed)
+
+formated = format_parsed_file(parsed)
+print(formated)
