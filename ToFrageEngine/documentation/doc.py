e01073034a9d7a06ee54b3c849fb0cdeb2570ffa
Maddix <starcraftd@gmail.com>
2015-12-15 01:55:49 -0500

    Another try at the Javascript parsing.
    I need to just buckle down and learn how to use a lex parser.

--- ToFrageEngine/documentation/doc.py
+++ ToFrageEngine/documentation/doc.py
@@ -1,7 +1,5 @@
 #! /usr/bin/python3
 
-import sys
-
 # Still a work in progress
 
 def load_file(path, filename, operation="r"):
@@ -10,94 +8,49 @@ def load_file(path, filename, operation="r"):
 		file = open_file.readlines()
 	return file
 
-# This is the sketchyest code I have ever written. I feel ashamed. It does work though.
-def parse_new(loaded_file):
-	#print(loaded_file)
-	cleaned = []
-	for line in loaded_file:
-		striped = line.strip().split("//")[0]
-		if striped != "":
-			cleaned.append(striped)
-
-	level = 0
-	group = {}
-	depth = []
-	ignore = False
-	has_local = False
-	get_local_defaults = False
-	level_map = []
-
-	for line in cleaned:
-		if line.find("{") != -1:
-			level += 1
-			level_map.append(level)
-		if line.find("}") != -1:
-			level -= 1
-			level_map.append(level)
-
-		if line.count("function ") and not ignore:
-			marker = group
-
-			for foot in depth:
-				marker = marker[foot]
-			marker[line] = {}
-			depth.append(line)
-			print(line)
-
-		if line.count("localContainer = "):
-			marker = group
-			for foot in depth:
-				marker = marker[foot]
-			print(line)
-
-		if line.count("localContainer.") and line.count("= function") and not line.count("var"):
-			marker = group
-			for foot in depth:
-				marker = marker[foot]
-			local_name = line.split(" ")[0]
-			print("->", local_name)
-			marker[local_name] = []
-
-		if get_local_defaults:
-			marker = group
-			for foot in depth:
-				marker = marker[foot]
-			#print(marker)
-			marker[0].append(line)
-			print("- - ->", line)
-
-		if line.count("var local ="):
-			get_local_defaults = True
-			marker = group
-			for foot in depth:
-				marker = marker[foot]
-			marker[local_name].append([])
-			depth.append(local_name)
+def has(line, items):
+	return [line.index(item) if item in line else -1 for item in items]
 
 
-		if get_local_defaults and line.count("};"):
-			get_local_defaults = False
+def isNamedFunction(line):
+	indexs = has(line, ["function", "(", ")"])
+	if not -1 in indexs:
+		name = line[indexs[0]+len("function"):indexs[1]].strip()
+		if name == "":
+			return False
+		params = line[indexs[1]:indexs[2]].split(",")
+		return (name, params)
+	return False
 
-		if line.count("local.") and line.count("= function"):
-			marker = group
-			for foot in depth:
-				marker = marker[foot]
-			marker.append(line)
-			print("- ->", line)
-			ignore = True
+def isGroupFunction(line, group_name):
+	indexs = has(line, [group_name, "function", "=", "(", ")", "{"])
+	if not -1 in indexs:
+		name = line[indexs[0]+len(group_name)+1:indexs[2]].strip()
+		params = line[indexs[3]+1:indexs[4]].split(",")
+		return (name, params)
+	return False
 
-		if line.count("return local;"):
-			ignore = False
-			depth.pop()
+def parse(lines, container=False):
+	line = lines.pop(0).strip()
 
-		if line.count("return localContainer") and not line.count("="):
-			depth.pop()
-			print(line)
+	named = isNamedFunction(line)
+	if named:
+		return named
+	group = isGroupFunction(line, "localConainer")
+	sub = isGroupFunction(line, "local")
+	if group:
+		return group
+	elif sub:
+		return sub
 
-	# I don't think I need levels..
-	print(level_map)
 
-	return group
+lines = load_file("", "test_file.txt")
 
-loaded = load_file("", "test_file.txt")
-print(parse_new(loaded))
+count = 0
+while lines != [] or count == 300:
+	result = parse(lines)
+	if result:
+		print(count, ":", result)
+	count += 1
+
+
